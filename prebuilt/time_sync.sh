34b8497727081f6f157e76d5914279b965e28d3c
jefby <jef199006@gmail.com>
2026-04-18 20:36:04 +0800

    [feat]: update timesync logic

--- prebuilt/time_sync.sh
+++ prebuilt/time_sync.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-SLEEP_INTERVAL=120 # 120秒检查一次
+SLEEP_INTERVAL=30 # 初始检查时间间隔为30秒
 while true; do
     CUR_TIME=$(date)
-    echo "Current time: $CUR_TIME. Synchronizing time with NTP server..."
-    ntpdate -u ntp.aliyun.com time1.cloud.tencent.com > /tmp/ntp_sync.log 2>&1
-    if [ $? -eq 0 ]; then
-        echo "Time synchronization successful."
+
+    if [ $CUR_TIME > '1980-01-01 00:00:00' ]; then
+        echo "System time is valid: $CUR_TIME" >> /tmp/time_sync.log
+        SLEEP_INTERVAL=3600 # 时间有效，将检查间隔调整为1小时
+        echo "Next time check in 1 hour." >> /tmp/time_sync.log
     else
-        echo "Time synchronization failed. Check /tmp/ntp_sync.log for details."
+        echo "System time is invalid: $CUR_TIME. Synchronizing time with NTP server..."
+        ntpdate -u ntp.aliyun.com time1.cloud.tencent.com > /tmp/ntp_sync.log 2>&1
+        if [ $? -eq 0 ]; then
+            echo "Time synchronization successful."   >> /tmp/ntp_sync.log 
+        else
+            echo "Time synchronization failed. Check /tmp/ntp_sync.log for details."  >> /tmp/ntp_sync.log
+        fi
     fi
     
     sleep $SLEEP_INTERVAL
